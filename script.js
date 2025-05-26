9ed367a730402e0064533a4789464c9a884b1527
Lucas Barbosa <lucas2304barbosa@gmail.com>
2025-05-26 09:30:46 -0300

    feito modal

--- script.js
+++ script.js
@@ -121,18 +121,21 @@ function preencherModal(elemento) {
   tabelaPeriodica.style.justifyContent = 'left';
   const modal = document.querySelector('#modalDados');
   modal.style.display = 'flex';
-  
-  document.querySelector('#nome').innerHTML = `${elemento.nome}<sup id="num-atomico">${elemento.numeroAtomico}</sup>`;
-  document.querySelector('#simbolo').innerText = elemento.simbolo;
-  document.querySelector('#massa-atomica').innerText = elemento.massaAtomica;
-  document.querySelector('#ano-descoberda').innerText = elemento.anoDeDescoberta;
-  document.querySelector('#grupo').innerText = elemento.grupo;
-  document.querySelector('#estado-padrao').innerText = elemento.estadoPadrao;
-  document.querySelector('#tipo-ligacao').innerText = elemento.tipoDeLigacao;
-  document.querySelector('#afinidade-eletrica').innerText = elemento.afinidadeEletronica;
-  document.querySelector('#eletronegatividade').innerText = elemento.eletronegatividade;
-  document.querySelector('#ponto-fusao').innerText = elemento.pontoDeFusao;
-  document.querySelector('#config-eletronica').innerText = elemento.configuracaoEletronica;
+
+  // Se o valor do dado for inexistente (undefined, null ou vazio), mostra "sem informações" no lugar
+  document.querySelector('#nome').innerHTML = `${elemento.nome || "sem informações"}<sup id="num-atomico">${elemento.numeroAtomico || ""}</sup>`;
+  document.querySelector('#simbolo').innerHTML = `<p class="identifica-dados">Símbolo:</p> ${elemento.simbolo || "sem informações"}`;
+  document.querySelector('#massa-atomica').innerHTML = `<p class="identifica-dados">Massa atômica:</p> ${elemento.massaAtomica || "sem informações"}`;
+  document.querySelector('#ano-descoberda').innerHTML = `<p class="identifica-dados">Ano de descoberta:</p> ${elemento.anoDeDescoberta || "sem informações"}`;
+  document.querySelector('#grupo').innerHTML = `<p class="identifica-dados">Grupo:</p> ${elemento.grupo || "sem informações"}`;
+  document.querySelector('#estado-padrao').innerHTML = `<p class="identifica-dados">Estado padrão:</p> ${elemento.estadoPadrao || "sem informações"}`;
+  document.querySelector('#tipo-ligacao').innerHTML = `<p class="identifica-dados">Tipo de ligação:</p> ${elemento.tipoDeLigacao || "sem informações"}`;
+  document.querySelector('#afinidade-eletrica').innerHTML = `<p class="identifica-dados">Afinidade elétrica:</p> ${elemento.afinidadeEletronica || "sem informações"}`;
+  document.querySelector('#eletronegatividade').innerHTML = `<p class="identifica-dados">Eletronegatividade:</p> ${elemento.eletronegatividade || "sem informações"}`;
+  document.querySelector('#ponto-fusao').innerHTML = `<p class="identifica-dados">Ponto de fusão:</p> ${elemento.pontoDeFusao || "sem informações"}`;
+  document.querySelector('#config-eletronica').innerHTML = `<p class="identifica-dados">Configuração eletrônica:</p> ${elemento.configuracaoEletronica || "sem informações"}`;
+
+
 }
 
 aplicarTabelaPeriodica(colecaoElementos);
